4af7120563a1e93db17b41004f0f289f81710d75
Nikolaj Skovmose <skovmissen@gmail.com>
2017-10-27 14:24:05 +0200

    api + test

---
+++ UnittestBudgetManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnittestBudgetManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnittestBudgetManager")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f12e549-c867-482f-bf90-4274f2efc861")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnittestBudgetManager/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BudgetManagerV2.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+using BudgetManagerV2.Models;
+using System.Collections.Generic;
+
+namespace UnittestBudgetManager
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetTransaction()
+        {
+            foreach (var item in GetTestTransaction())
+            {
+               
+                var transaction = new
+                {
+                    ID = item.Id,
+                    Text = item.Text,
+                    Value = item.Value,
+
+                };
+                Assert.AreNotEqual(transaction, null);
+            }
+           
+        }
+        public  List<Transaction> GetTestTransaction()
+        {
+            var testProducts = new List<Transaction>();
+            testProducts.Add(new Transaction { Id = 1, Value = 452525, Text = "Hej hej, dette er en test" });
+            testProducts.Add(new Transaction { Id = 2, Value = 452525, Text = "Hej hej, dette er en test" });
+            return testProducts;
+        }
+    }
+}
