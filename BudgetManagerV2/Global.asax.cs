855c4eca2f62754a0812beb4842cee48faaf683f
Nikolaj Skovmose <skovmissen@gmail.com>
2017-10-27 14:22:10 +0200

    Api er mega done + tests

--- BudgetManagerV2/Global.asax.cs
+++ BudgetManagerV2/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace BudgetManagerV2
 {
@@ -12,10 +14,17 @@ namespace BudgetManagerV2
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+            // Register Web API routing support before anything else
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // The rest of our file goes here
+            // ...
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // ...
         }
     }
 }
