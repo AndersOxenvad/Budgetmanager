5b528f863df6567e9485e651f0d809e819164341
Nikolaj Skovmose <skovmissen@gmail.com>
2017-10-31 09:33:54 +0100

    list and general info

---
+++ BudgetManagerV2/Controllers/TransactionApiController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using BudgetManagerV2.Models;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using System;
+
+namespace BudgetManagerV2.Controllers
+{
+
+    public class TransactionController : ApiController
+    {
+
+        private BudgetManagerEntities db = new BudgetManagerEntities();
+        Transaction trans = new Transaction();
+        Category cat = new Category();
+
+        public IHttpActionResult GetTransaction(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+
+
+            foreach (var item in db.Transaction.Include(t => t.Category).Where(x => x.Id == id))
+            {
+
+                var transaction = new
+                {
+                    ID = item.Id,
+                    Text = item.Text,
+                    Value = item.Value,
+                    Date = item.Date,
+                    Category = item.Category.Name
+                };
+                if (transaction == null)
+                {
+                    return Redirect("~/Views/Home/index.cshtml");
+                }
+                return Ok(JsonConvert.SerializeObject(transaction));
+            }
+
+            return BadRequest("No transactions with that id");
+        }
+        public IHttpActionResult GetTransactionList(string start, string end)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+            List<object> transactionList = new List<object>();
+
+            start += "T00:00:00";
+            end += "T00:00:00";
+
+            DateTime? startTime = Convert.ToDateTime(start);
+            DateTime? endTime = Convert.ToDateTime(end);
+         
+            
+
+            foreach (var item in db.Transaction.Include(t => t.Category).Where(x => x.Date >= startTime && x.Date <= endTime))
+            {
+                var transaction = new
+                {
+                    ID = item.Id,
+                    Text = item.Text,
+                    Value = item.Value,
+                    Date = item.Date,
+                    Category = item.Category.Name
+                };
+                if (transaction == null)
+                {
+                    return Redirect("~/Views/Home/index.cshtml");
+                }
+                transactionList.Add(transaction);
+                
+            }
+            if (transactionList == null)
+            {
+                return BadRequest("Request error");
+            }
+            return Ok(JsonConvert.SerializeObject(transactionList));
+
+        }
+    }
+}
