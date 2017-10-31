3a205e410701fb9fa8c38f3b96e8540ee6e0b8d9
Nikolaj Skovmose <skovmissen@gmail.com>
2017-10-31 10:42:26 +0100

    Done with transaction list and general info

--- UnittestBudgetManager/UnitTest1.cs
+++ UnittestBudgetManager/UnitTest1.cs
@@ -22,17 +22,45 @@ namespace UnittestBudgetManager
                     ID = item.Id,
                     Text = item.Text,
                     Value = item.Value,
+                    Date = item.Date
 
                 };
                 Assert.AreNotEqual(transaction, null);
             }
            
         }
+        [TestMethod]
+        public void GetTransactionList()
+        {
+            string startTime = "2017-01-01";
+            string endTime = "2017-02-06";
+
+            DateTime? start = Convert.ToDateTime(startTime);
+            DateTime? end = Convert.ToDateTime(endTime);
+            List<object> transList = new List<object>();
+            List < Transaction > testItems = GetTestTransaction().FindAll(x => x.Date >= start && x.Date <= end);
+            foreach (var item in testItems)
+            {
+
+                var transaction = new
+                {
+                    ID = item.Id,
+                    Text = item.Text,
+                    Value = item.Value,
+                    Date = item.Date
+
+                };
+                transList.Add(transaction);
+                
+            }
+            Assert.AreEqual(transList.Count, 2);
+
+        }
         public  List<Transaction> GetTestTransaction()
         {
             var testProducts = new List<Transaction>();
-            testProducts.Add(new Transaction { Id = 1, Value = 452525, Text = "Hej hej, dette er en test" });
-            testProducts.Add(new Transaction { Id = 2, Value = 452525, Text = "Hej hej, dette er en test" });
+            testProducts.Add(new Transaction { Id = 1, Value = 452525, Text = "Hej hej, dette er en test", Date = Convert.ToDateTime("2017-01-02") });
+            testProducts.Add(new Transaction { Id = 2, Value = 452525, Text = "Hej hej, dette er en test", Date = Convert.ToDateTime("2017-02-05") });
             return testProducts;
         }
     }
